74b023a8bd561de195cef266d81f726725947ec9
Marco Almazan <markosayajin@hotmail.com>
2023-05-02 00:22:09 -0600

    Faces

--- image_processing.py
+++ image_processing.py
@@ -1,9 +1,9 @@
 from sklearn.decomposition import PCA
 from sklearn.decomposition import TruncatedSVD
 from sklearn.manifold import Isomap
+from sklearn.metrics.pairwise import cosine_similarity
 #from sklearn.manifold import TSNE
 #from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from matplotlib import pyplot
 
 import face_recognition
 from create_database import image_to_vector
@@ -22,47 +22,37 @@ def classify_faces(images_dataframe, profile):
     #print(face.shape)
     face=numpy.asarray(images_dataframe.iloc[:,1:])
 
-    dimentionality_reduction(face, profile)
+    dimentionality_reduction(face, profile, images_dataframe)
 
-def image_reduction(vectorized_image):
-    Models = {
-    "PCA":PCA(n_components = 2),
-    "SVD":TruncatedSVD(n_components = 2),
-    #"TSNE": TSNE(n_components = 2)
-    }
-
-    for model_tag in Models:
-        #print(model_tag)
-        Model = Models.get(model_tag)
-        #images_array_model = Model.fit_transform(vectorized_image)
-        #reduced_face = images_array_model.transform(images_array)
-
-        if model_tag == "PCA": #JALA
-            reduced_face = Model.transform(vectorized_image)
-            pca.append(reduced_face)
-        if model_tag == "SVD": #JALA
-            reduced_face = Model.transform(vectorized_image)
-            svd.append(reduced_face)
-        if model_tag == "TSNE": #JALA
-            images_array_model = Model.fit_transform(vectorized_image)
-            tsne.append(images_array_model)
-
-def dimentionality_reduction(images_array, vectorized_image):
+def dimentionality_reduction(images_array, vectorized_image, image_dataframe):
     Models = {
     "PCA":PCA(n_components = 2),
     "SVD":TruncatedSVD(n_components = 2),
     #"TSNE": TSNE(n_components = 2)
     "ISOmap": Isomap(n_components = 2)
     }
-
+    
     for model_tag in Models:
         #print(model_tag)
         Model = Models.get(model_tag)
         images_array_model = Model.fit(images_array)
         reduced_faces = images_array_model.transform(images_array)
         reduced_face = images_array_model.transform(vectorized_image)
-        print(reduced_face)
+        #print(reduced_face)
+        similarities = []
+        i = 0
+        for db_face in reduced_faces:
+            #similarity = numpy.linalg.norm(reduced_face[0]-db_face)
+            similarity = DistCos(reduced_face[0], db_face)
+            similarities.append(similarity)
+            print(i, similarity)
+            i += 1
 
+        #print('-------------------')
+        #similarities = numpy.asarray(similarities)
+        #Idx = numpy.argsort(similarities)
+        #print(image_dataframe.iloc[Idx[0:5]])
+        print('-----------------------')
         #if model_tag == "PCA": #JALA
         #    pca.append(images_array_model)
         #if model_tag == "SVD": #JALA
@@ -72,11 +62,22 @@ def dimentionality_reduction(images_array, vectorized_image):
 
     #pyplot.show()
     #print(numpy.asareductions[0])
-    print("#####")
+    #print("#####")
     #print(reductions)
 
-def similarity_comparison():
-    pass
+def L2(x,y):
+  z = x-y
+  z = z ** 2
+  z = numpy.sum(z)
+  return numpy.power(z,1/2)
+
+def DistCos(x,y):
+  a = numpy.matmul(x.T, y)
+  b = numpy.power( numpy.matmul( x.T, x), 1/2 ) * numpy.power( numpy.matmul( y.T, y), 1/2 )
+  return a/b  
+
+def similarity_comparison(x,y, kernel):
+    return kernel(x,y)
 
 images = glob.glob("files/TC3002B_Faces/" + "**/**.jpg")
 df=pandas.read_csv("Faces.csv")
@@ -96,7 +97,9 @@ tsne = []
 #print(tsne)
 
 #images[0] es una ruta de prueba para la vectorizacion y reduccion individual de dimensiones de imagen
-vectorized_image=image_to_vector(images[0])
+print(images[2])
+print('-----------------')
+vectorized_image=image_to_vector(images[2])
 #image_reduction(numpy.asarray(vectorized_image).reshape(1, -1))
 
 classify_faces(df,numpy.asarray(vectorized_image).reshape(1, -1))
