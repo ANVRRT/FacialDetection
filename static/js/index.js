356843fab7e3b74b39e399d6d0454a32b25d3e4f
anvrrt <albertonavarreteramirez@gmail.com>
2023-05-03 21:46:39 -0600

    Added carrousel

--- static/js/index.js
+++ static/js/index.js
@@ -124,30 +124,3 @@
   // once loading is complete.
   window.addEventListener('load', startup, false);
 
-
-  function switchImage(number) {
-    switch(number) {
-        case 1:
-            var replacement = document.getElementById("mini1").src;
-            var original = document.getElementById("mainSlide").src;
-            document.getElementById("mini1").src = original;
-            document.getElementById("mainSlide").src = replacement;
-            break;
-        case 2:
-            var replacement = document.getElementById("mini2").src;
-            var original = document.getElementById("mainSlide").src;
-            document.getElementById("mini2").src = original;
-            document.getElementById("mainSlide").src = replacement;
-            break;
-        case 3:   
-            var replacement = document.getElementById("mini3").src;
-            var original = document.getElementById("mainSlide").src;
-            document.getElementById("mini3").src = original;
-            document.getElementById("mainSlide").src = replacement;
-            break;
-        default:
-            var replacement = document.getElementById("mainSlide").src;
-            document.getElementById("mainSlide").src = replacement;
-            break;
-    }
-}
